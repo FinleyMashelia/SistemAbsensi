4d8c131f7af39f220ee831a3464651f4ac54dfcc
FinleyMashelia <92424796+FinleyMashelia@users.noreply.github.com>
2023-02-22 18:49:09 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('ajikdb.mysql.database.azure.com','ajikdb','','Batassuci123');
+$conn = mysqli_connect('ajikdb.mysql.database.azure.com','ajikdb','Batassuci123','user_db');
 
 if (mysqli_connect_errno()){
     echo "Koneksi database gagal : " . mysqli_connect_error();
